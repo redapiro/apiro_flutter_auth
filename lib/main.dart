1a6612e36790462540ba87f3322d50dc0f5e4638
singdev20 <singdev@apiro.com.sg>
2021-06-16 15:20:33 +0530

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:user_authentication/utils/app_colors.dart';
 import 'package:user_authentication/widget/login/login_screen.dart';
 
 void main() {
@@ -12,16 +13,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Apiro Flutter Auth',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        primaryColor: AppColors.primaryColor,
+        backgroundColor: Colors.red,
+        accentColor: Colors.red,
+        dividerColor: AppColors.dividerColor,
+        scaffoldBackgroundColor: Colors.white,
+        disabledColor: AppColors.disabledColor,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+            buttonColor: Colors.white,
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            )),
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                fontFamily: "OpenSans",
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          color: AppColors.primaryColor,
+          textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 20,
+                // color: Colors.blueGrey[600],
+                color: Colors.white,
+                // fontWeight: FontWeight.w700,
+              )),
+        ),
       ),
       home: LoginScreen(),
     );
